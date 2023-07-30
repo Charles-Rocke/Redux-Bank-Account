fb4193ae6f6115ac468adb690704ed10b52839d9
SpOOk <spook.devv@gmail.com>
2023-07-30 13:41:00 -0400

    Initial Working with Redux in Isolation commit

---
+++ src/store.js
@@ -0,0 +1,29 @@
+// Learn redux in isolation
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return;
+      // if
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    // in redux Dont throw a new error
+    default:
+      return state;
+  }
+}

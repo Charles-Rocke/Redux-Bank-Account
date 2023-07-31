852055fd4bc6adc211c16d212df4429bfb82477b
SpOOk <spook.devv@gmail.com>
2023-07-31 11:01:11 -0400

    Providing store to app, useSelector to call customer data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 
 // run code that is in the store.js file
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 350000 });
-console.log(store.getState());
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

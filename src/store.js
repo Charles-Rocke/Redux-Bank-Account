51b4c42c02a4df139048491c463c0949e92e85fc
SpOOk <spook.devv@gmail.com>
2023-07-31 08:43:18 -0400

    Added action creators

--- src/store.js
+++ src/store.js
@@ -38,21 +38,53 @@ function reducer(state = initialState, action) {
 const store = createStore(reducer);
 
 // dispatch actions from store
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
 
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: {
+//     amount: 1000,
+//     loanPurpose: "Buy Tailwind",
+//   },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+// sction creators
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount: amount,
+      loanPurpose: purpose,
+    },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
-store.dispatch({ type: "account/withdraw", payload: 200 });
 
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    loanPurpose: "Buy Tailwind",
-  },
-});
+store.dispatch(requestLoan(1000, "Buy Luck"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
